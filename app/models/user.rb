564d7922a49e59ec9ea1d3b48c9672b8dac328d8
EC2 Default User <ec2-user@ip-172-31-27-141.ec2.internal>
2021-10-03 04:06:08 +0000

    add いいね機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 
   has_many :book_comments, dependent: :destroy
 
+  has_many :favorites, dependent: :destroy
+
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 end
